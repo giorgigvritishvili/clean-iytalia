220dcf299f7b76144cb3bf19c9654528f3c9b657
giorgigvritishvili <gvritishvilig720@gmail.com>
2026-01-13 22:10:16 +0400

    commit

--- server.js
+++ server.js
@@ -747,7 +747,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/admin', (req, res) => {
+app.get('/admin', requireAdmin, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
