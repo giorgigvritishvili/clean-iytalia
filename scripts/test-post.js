05e8a6d85cd1babe0f38832e76c6877adc6d971c
giorgigvritishvili <gvritishvilig720@gmail.com>
2026-02-14 15:37:17 +0400

    commit

--- scripts/test-post.js
+++ scripts/test-post.js
@@ -22,7 +22,7 @@
     };
 
     const PORT = process.env.PORT || 5000;
-    const res = await fetch(`http://localhost:${PORT}/api/bookings`, {
+    const res = await fetch(`http://127.0.0.1:${PORT}/api/bookings`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(booking)
